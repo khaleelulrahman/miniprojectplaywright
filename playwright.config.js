7daac0a5dc1df707fe5450cb0a1a07756bb776fb
khaleelulrahman <kaleel15593@gmail.com>
2026-07-21 18:08:58 +0530

    minor changes

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-reporter: [
+reporter: [['github'],
   ['allure-playwright', {
     outputFolder: 'allure-results'
   }]
